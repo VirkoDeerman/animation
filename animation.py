d7f54db357a72e83347c24407a4cc84924acd7f1
Virko Deerman <robertviva2019@gmail.com>
2025-11-14 14:28:37 +0100

    Refactor String class initialization and properties

--- animation.py
+++ animation.py
@@ -1,50 +1,48 @@
 from time import sleep
 from copy import deepcopy
 
+
 class String:
-	def __init__(self, length: int = 20):
-		self.__string:	 list[str] = list('-' * self.__length)
-		self.__forwards: 	  bool = True
-		self.__pos:			   int = 0
-		if length > 5:
-			self.__length:	   int = length
-		else:
-			self.__length:	   int = 5
-        
-	@property
-	def pos(self) -> int:
-		self.__pos += self.step
-		return self.__pos
+    def __init__(self, length: int = 20):
+        if length > 5:
+            self.__length: int = length
+        else:
+            self.__length: int = 5
+        self.__string: list[str] = list('-' * self.__length)
+        self.__forwards: bool = True
+        self.__pos: int = 0
 
-	@pos.setter
-	def pos(self, pos: int) -> None:
-		if self.__pos < 0:
-			self.__pos = 0
-		elif self.__pos >= self.__length:
-			self.__pos = self.__length - 1
-		else:
-			self.__pos = pos
-       
-	@property
-	def step(self) -> int:
-		return 1 if self.__forwards else -1
+    @property
+    def pos(self) -> int:
+        self.__pos += self.step
+        return self.__pos
 
-	@property
-	def string(self) -> str:
-		string = deepcopy(self.__string)
-		below_zero = self.__pos + self.step < 0
-		too_far = self.__pos + self.step >= self.__length
-		if below_zero or too_far:
-			self.__forwards = not self.__forwards
-		string[self.pos] = 'O'
-		return ''.join(i for i in string)
+    @pos.setter
+    def pos(self, pos: int) -> None:
+        if self.__pos < 0:
+            self.__pos = 0
+        elif self.__pos >= self.__length:
+            self.__pos = self.__length - 1
+        else:
+            self.__pos = pos
 
+    @property
+    def step(self) -> int:
+        return 1 if self.__forwards else -1
 
-a: String = String()
+    @property
+    def string(self) -> str:
+        string = deepcopy(self.__string)
+        below_zero = self.__pos + self.step < 0
+        too_far = self.__pos + self.step >= self.__length
+        if below_zero or too_far:
+            self.__forwards = not self.__forwards
+        string[self.pos] = 'O'
+        return ''.join(i for i in string)
 
 
-how_long: int(print('How long do you want for animation to play in seconds?\n> '))
+how_long: int = int(input('How long do you want for animation to play in seconds?\n> '))
 print()
 for i in range(how_long * 10):
-	print('\r' + a.string, end = '')
-	sleep(0.1)
+    print('\r' + a.string, end='')
+    sleep(0.1)
